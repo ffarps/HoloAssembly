084c3b735ab84518cc82007b153d49ab0a99d2e2
Francisco André <franciscoandre.farp@gmail.com>
2023-10-02 03:19:46 +0100

    Update CubeInstantiator.cs

--- Assets/Scripts/CubeInstantiator.cs
+++ Assets/Scripts/CubeInstantiator.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
+using static UnityEngine.GraphicsBuffer;
 
-public class CubeInstantiator : MonoBehaviour
+public class CubeInstantiator : DefaultObserverEventHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //The MyPrefabInstantiator class inherits from the DefaultObserverEventHandler class, that takes care of handling events when the target is detected.At that point, the Prefab 3D model gets instantiated and gets attached on the Image Target.
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Cube;
+    GameObject NewCube;
+    public Vector3 CubePosition;
+    protected override void OnTrackingFound()
     {
-        
+        Debug.Log("Target Found");
+        if (NewCube == null)
+            InstantiateCubePrefab();
+        base.OnTrackingFound();
+    }
+    void InstantiateCubePrefab()
+    {
+        if(Cube!=null)
+        {
+            Debug.Log("Target found!, adding content");
+            NewCube = Instantiate(Cube, mObserverBehaviour.transform);
+            NewCube.transform.localPosition = CubePosition;
+            NewCube.SetActive(true);
+        }
     }
 }
