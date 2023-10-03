503b35dc32f9c7b62bbd69ab1ce69322c3963ed4
Francisco André <franciscoandre.farp@gmail.com>
2023-10-03 19:21:47 +0100

    update UI

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class LoadScene : MonoBehaviour
 {
     bool triggered;
     public bool loadSampleSceneImageTarget;
-
     public void LoadImageTargetScene()
     {
         SceneManager.LoadScene("SampleSceneImageTarget");
+    }
+    public void quitAppNow()
+    {
+        Application.Quit();
     }
 }
