21a3875fe3070023a45b94110584842da48855a5
Francisco André <franciscoandre.farp@gmail.com>
2023-10-14 21:12:08 +0100

    update gazePositionManager

---
+++ Assets/gazePositionManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gazePositionManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log(Screen.width); Debug.Log(Screen.height);
+        var phoneCenterOffsetWidth = Screen.width/2;Debug.Log(phoneCenterOffsetWidth);
+        var phoneCenterOffsetHeight = Screen.height/2;Debug.Log(phoneCenterOffsetHeight);
+        transform.localPosition= new Vector3(phoneCenterOffsetWidth, phoneCenterOffsetHeight);
+    }
+}
