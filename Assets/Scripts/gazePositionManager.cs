04c9a4c1faebc21a859fc4265fe254fc81679da4
Francisco André <franciscoandre.farp@gmail.com>
2023-10-14 21:25:44 +0100

    update Interaction Logic

--- Assets/Scripts/gazePositionManager.cs
+++ Assets/Scripts/gazePositionManager.cs
@@ -6,9 +6,9 @@ public class gazePositionManager : MonoBehaviour
 {
     private void Awake()
     {
-        Debug.Log(Screen.width); Debug.Log(Screen.height);
-        var phoneCenterOffsetWidth = Screen.width/2;Debug.Log(phoneCenterOffsetWidth);
-        var phoneCenterOffsetHeight = Screen.height/2;Debug.Log(phoneCenterOffsetHeight);
+        //Debug.Log(Screen.width); Debug.Log(Screen.height);
+        var phoneCenterOffsetWidth = Screen.width/2;//Debug.Log(phoneCenterOffsetWidth);
+        var phoneCenterOffsetHeight = Screen.height/2;//Debug.Log(phoneCenterOffsetHeight);
         transform.localPosition= new Vector3(phoneCenterOffsetWidth, phoneCenterOffsetHeight);
     }
 }
