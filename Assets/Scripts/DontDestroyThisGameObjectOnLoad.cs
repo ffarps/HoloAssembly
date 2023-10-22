5acb6616079b15e072b21a424258f74feffffe49
Francisco André <franciscoandre.farp@gmail.com>
2023-10-22 18:51:42 +0100

    update gaze manager

--- Assets/Scripts/DontDestroyThisGameObjectOnLoad.cs
+++ Assets/Scripts/DontDestroyThisGameObjectOnLoad.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 public class DontDestroyThisGameObjectOnLoad : MonoBehaviour
 {
-    //this script is used in every scene in which vuforia is running, where if the user presses the go back move/button, the app should inform the user to go back to the GUI scene
-void Awake()
+    ///summary
+    ///this script is used in every scene in which vuforia is running, where if the user presses the go back move/button, the app should inform the user to go back to the GUI scene <summary>
+    /// </summary>
+    void Awake()
 {
         // Make the GameObject persist across scenes
         DontDestroyOnLoad(this.gameObject);
--- Assets/Scripts/InteractionManagerScript.cs
+++ Assets/Scripts/InteractionManagerScript.cs
@@ -1,42 +1,68 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 public class InteractionManagerScript : MonoBehaviour
 {
+    /// <summary>
+    /// This class handles the interaction between objects and a cast ray
+    /// 
+    /// ffarps October 2023
+    /// </summary>
     public Transform MainCameraObject;
-    public float distance;
+    float distanceToObject = 10f;
     public TextMeshProUGUI textMeshProUGUI;
-    //public Material highlightMaterial;
     Outline outline;
     public Material originalMaterial;
     bool hasAnyObjectBeenSelected = false;
     void Update()
     {
         Ray ray = new Ray(MainCameraObject.position, MainCameraObject.forward);
-        Debug.DrawRay(MainCameraObject.position, -MainCameraObject.up * distance, Color.green);
+        Debug.DrawRay(MainCameraObject.position, -MainCameraObject.up * distanceToObject, Color.green);
         RaycastHit hit;
         //this will detect if the game object is hit by the ray
-        if (Physics.Raycast(ray, out hit, distance))
+        if (Physics.Raycast(ray, out hit, distanceToObject))
         {
-            if (hit.transform.TryGetComponent<Outline>(out outline))
-            {
-                while (outline.OutlineWidth < 5)
-                {
-                    outline.OutlineWidth++;
-                }
-                hasAnyObjectBeenSelected = true;
-                textMeshProUGUI.text = hit.transform.name;
-            }
-            //if (hit.collider.CompareTag("Interactive"))
             //action to be perform by selecting the Interactible object
-            //Debug.LogWarning("O objeto "+hit.transform.name+ " foi colidido");
+            OnHover(hit);
+            //Debug.Log("O objeto "+hit.transform.name+ " foi colidido");
         }
         else if (hit.collider == null && hasAnyObjectBeenSelected)
         {
-            textMeshProUGUI.text = "";
-            while (outline.OutlineWidth > 0)
+            OnExit();
+        }
+    }
+    void OnHover(RaycastHit hit)
+    {
+        if (hit.transform.TryGetComponent<Outline>(out outline))
+        {
+            while (outline.OutlineWidth < 5)
             {
-                outline.OutlineWidth--;
+                outline.OutlineWidth++;
+            }
+            hasAnyObjectBeenSelected = true;
+            textMeshProUGUI.text = hit.transform.name;
+            if(Input.GetMouseButtonDown(0))
+            {
+                Debug.LogWarning("click!");
             }
         }
+    }
+    void OnExit()
+    {
+        textMeshProUGUI.text = "";
+        while (outline.OutlineWidth > 0)
+        {
+            outline.OutlineWidth--;
+        }
+    }
+    void OnSelect()
+    {
+
+    }
+    void OnDeselect()
+    {
+
     }
 }
