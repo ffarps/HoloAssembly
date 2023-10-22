99e8649ebeed30ba90603e6f6a303f1c279c7898
Francisco André <franciscoandre.farp@gmail.com>
2023-10-22 03:21:11 +0100

    update Interaction-able objects and methods

--- Assets/Scripts/InteractionManagerScript.cs
+++ Assets/Scripts/InteractionManagerScript.cs
@@ -5,31 +5,46 @@ public class InteractionManagerScript : MonoBehaviour
     public Transform MainCameraObject;
     public float distance;
     public TextMeshProUGUI textMeshProUGUI;
-    public Material highlightMaterial;
-    GameObject selectedObject;
+    //public Material highlightMaterial;
+    Outline outline;
     public Material originalMaterial;
     bool hasAnyObjectBeenSelected = false;
     void Update()
     {
         Ray ray = new Ray(MainCameraObject.position, MainCameraObject.forward);
         Debug.DrawRay(MainCameraObject.position, -MainCameraObject.up * distance, Color.green);
-
         RaycastHit hit;
         //this will detect if the game object is hit by the ray
         if (Physics.Raycast(ray, out hit, distance))
         {
-            if (hit.collider.CompareTag("Interactive"))
+            //estes ifs podem nao fazer sentido
+            if (outline != null)
             {
-                hasAnyObjectBeenSelected = true;
+                if (outline.transform != hit.transform)
+                {
+                    outline = null;
+                }
             }
-            textMeshProUGUI.text = hit.transform.name;
-
+            if (hit.transform.TryGetComponent<Outline>(out outline))
+            {
+                while (outline.OutlineWidth < 10)
+                {
+                    outline.OutlineWidth++;
+                }
+                hasAnyObjectBeenSelected = true;
+                textMeshProUGUI.text = hit.transform.name;
+            }
+            //if (hit.collider.CompareTag("Interactive"))
             //action to be perform by selecting the Interactible object
             //Debug.LogWarning("O objeto "+hit.transform.name+ " foi colidido");
         }
         else if (hit.collider == null && hasAnyObjectBeenSelected)
         {
             textMeshProUGUI.text = "";
+            while (outline.OutlineWidth > 0)
+            {
+                outline.OutlineWidth--;
+            }
         }
     }
 }
