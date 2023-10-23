68bab617800f51ed5f1f3d647da295dbe4148903
Francisco André <franciscoandre.farp@gmail.com>
2023-10-23 16:41:29 +0100

    Update InteractionManagerScript.cs

--- Assets/Scripts/InteractionManagerScript.cs
+++ Assets/Scripts/InteractionManagerScript.cs
@@ -7,49 +7,57 @@ public class InteractionManagerScript : MonoBehaviour
 {
     /// <summary>
     /// This class handles the interaction between objects and a cast ray
-    /// 
     /// ffarps October 2023
     /// </summary>
-    public Transform MainCameraObject;
-    float distanceToObject = 10f;
-    public TextMeshProUGUI textMeshProUGUI;
     Outline outline;
-    public Material originalMaterial;
-    bool hasAnyObjectBeenSelected = false;
+    float DistanceToObject = 10f;
+    public Transform MainCameraObject;
+    public TextMeshProUGUI textMeshProUGUI;
+    GameObject HoveredObject;
+    GameObject SelectedObject;
+    bool hasAnyObjectBeenHovered = false;
     void Update()
     {
         Ray ray = new Ray(MainCameraObject.position, MainCameraObject.forward);
-        Debug.DrawRay(MainCameraObject.position, -MainCameraObject.up * distanceToObject, Color.green);
+        Debug.DrawRay(MainCameraObject.position, -MainCameraObject.up * DistanceToObject, Color.green);
         RaycastHit hit;
         //this will detect if the game object is hit by the ray
-        if (Physics.Raycast(ray, out hit, distanceToObject))
+        if (Physics.Raycast(ray, out hit, DistanceToObject))
         {
-            //action to be perform by selecting the Interactible object
-            OnHover(hit);
+            if (hit.transform.TryGetComponent<Outline>(out outline))
+                //action to be perform by selecting the Interactible object
+                OnHover(hit);
             //Debug.Log("O objeto "+hit.transform.name+ " foi colidido");
         }
-        else if (hit.collider == null && hasAnyObjectBeenSelected)
+        else if (hit.collider == null && hasAnyObjectBeenHovered)
         {
-            OnExit();
+            OnHoverExit();
         }
     }
     void OnHover(RaycastHit hit)
     {
-        if (hit.transform.TryGetComponent<Outline>(out outline))
+        
         {
             while (outline.OutlineWidth < 5)
             {
                 outline.OutlineWidth++;
             }
-            hasAnyObjectBeenSelected = true;
+            hasAnyObjectBeenHovered = true;
             textMeshProUGUI.text = hit.transform.name;
+            HoveredObject=hit.collider.gameObject;
             if(Input.GetMouseButtonDown(0))
             {
-                Debug.LogWarning("click!");
+                Select(hit);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Deselect(hit);
+                }
             }
         }
+        /// to do
+        /// Database 
     }
-    void OnExit()
+    void OnHoverExit()
     {
         textMeshProUGUI.text = "";
         while (outline.OutlineWidth > 0)
@@ -57,12 +65,23 @@ public class InteractionManagerScript : MonoBehaviour
             outline.OutlineWidth--;
         }
     }
-    void OnSelect()
+    void Select(RaycastHit hit)
     {
-
+        //this is only called when the object is hovered an clicked to select
+        SelectedObject = hit.collider.gameObject;
+        Debug.LogWarning("Object "+SelectedObject.name+" Selected");
     }
-    void OnDeselect()
+    void Deselect(RaycastHit hit)
     {
-
+        //this is only called when the object is hovered an clicked to deselect
+        if (SelectedObject!=null)
+        {
+            while (outline.OutlineWidth > 0)
+            {
+            outline.OutlineWidth--;
+            }
+            SelectedObject = null;
+        }
+        Debug.LogWarning("Object " + SelectedObject.name + " deselected");
     }
 }
